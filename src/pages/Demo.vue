7a3accc4d36900c9741a0c0865058dc187905cb1
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-06 17:34:10 +0200

    [dev] (feat): add components demo page

---
+++ src/pages/Demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="getClass">
+    <AppLink to="components" append>Components Demo</AppLink>
+  </div>
+</template>
+
+<script>
+import Component from '/utils/component'
+
+export default new Component({
+  name: 'Demo',
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/demo/Components.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="getClass">
+    <Header s1>Header s1</Header>
+    <Header s2>Header s2</Header>
+    <Header s3>Header s3</Header>
+    <Header s4>Header s4</Header>
+    <Header s5>Header s5</Header>
+    <Paragraph>Paragraph</Paragraph>
+    <Button>Button</Button>
+  </div>
+</template>
+
+<script>
+import Component from '/utils/component'
+
+export default new Component({
+  name: 'ComponentDemo',
+})
+</script>
+
+<style lang="scss" scoped></style>
