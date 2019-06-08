292e801f920cdb3c7c30faf06a7d9de8e8e2edbd
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-08 23:12:38 +0200

    [dev] (feat): debug page

---
+++ src/pages/Debug.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="getClass">
+    <Header s2>Connection Info</Header>
+    connection: {{ connection }}
+  </div>
+</template>
+
+<script>
+import Component from '/utils/component'
+
+const connection =
+  navigator.connection || navigator.mozConnection || navigator.webkitConnection
+
+export default new Component({
+  name: 'Debug',
+  data: () => ({
+    connection: '',
+  }),
+  methods: {
+    getConnection() {
+      this.connection = JSON.stringify(connection)
+    },
+  },
+  mounted() {
+    this.getConnection()
+    connection.addEventListener('change', this.getConnection)
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
