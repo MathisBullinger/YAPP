aef69f2c027097c406302474970395968a1158ac
Mathis Bullinger <bullinger.mathis@gmail.com>
2019-06-30 01:42:01 +0200

    [signin] (feat): add login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="getClass">
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+  </div>
+</template>
+
+<script>
+import Component from '/scripts/component'
+
+export default new Component({
+  name: 'Login',
+})
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: calc(100% - 4rem);
+
+  .g-signin2 {
+    height: 2rem;
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Router from 'vue-router'
 import Home from '~/pages/Home'
+import Login from '~/pages/Login'
 import Demo from '~/pages/Demo'
 import ComponentDemo from '~/pages/demo/Components'
 import Debug from '~/pages/Debug'
@@ -9,6 +10,7 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', component: Home },
+    { path: '/login', component: Login },
     { path: '/demo', component: Demo },
     { path: '/demo/components', component: ComponentDemo },
     { path: '/debug', component: Debug },
