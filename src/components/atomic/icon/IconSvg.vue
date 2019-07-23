6a0f08c0dc0d5d8bb10841f3d39ce6ea240dd86b
MathisBullinger <mathis.bullinger@code.berlin>
2019-07-23 13:57:16 +0200

    [dev] (fix): git case-sensitive dir name

---
+++ src/components/atomic/icon/IconSvg.vue
@@ -0,0 +1,76 @@
+<template>
+  <svg
+    v-if="icon === 'library'"
+    class="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      class="icon-main"
+      d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"
+    />
+    <path fill="none" d="M0 0h24v24H0z" />
+  </svg>
+  <svg
+    v-else-if="icon === 'feed'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      class="icon-main"
+      d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z"
+    />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+  <svg
+    v-else-if="icon === 'search'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      class="icon-main"
+      d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+    />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+  <svg
+    v-else-if="icon === 'account'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      class="icon-main"
+      d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"
+    />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+  <p v-else>unknown icon</p>
+</template>
+
+<script>
+import Component from '~/scripts/component'
+
+export default new Component({
+  name: 'IconSvg',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.icon-main {
+  fill: white;
+}
+</style>
