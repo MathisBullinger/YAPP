59c1cca1c34dc90c359c6b62518e223e8ea49a50
MathisBullinger <mathis.bullinger@code.berlin>
2019-07-27 17:52:04 +0200

    [dev] (ref): rewrite icon component

--- src/components/atomic/Icon.vue
+++ src/components/atomic/Icon.vue
@@ -1,12 +1,18 @@
 <template>
-  <div :class="getClass" @click="$emit('click')">
-    <IconSvg :icon="name"></IconSvg>
-  </div>
+  <object
+    type="image/svg+xml"
+    :data="icon"
+    :style="opacity"
+    @load="objLoaded"
+    @click="$emit('click')"
+  ></object>
 </template>
 
 <script>
 import Component from '~/scripts/component'
-import IconSvg from './icon/IconSvg'
+import icons from '../../../data/icons/*.svg'
+
+const ICON_COLOR = '#000'
 
 export default new Component({
   name: 'Icon',
@@ -15,13 +21,27 @@ export default new Component({
       type: String,
       default: '',
     },
-    aria: {
-      type: String,
-      default: '',
+  },
+  data() {
+    return {
+      loaded: false,
+    }
+  },
+  computed: {
+    icon() {
+      return icons[this.name]
+    },
+    opacity() {
+      return {
+        opacity: this.loaded ? 1 : 0,
+      }
     },
   },
-  components: {
-    IconSvg,
+  methods: {
+    objLoaded() {
+      this.$el.getSVGDocument().querySelector('path').style.fill = ICON_COLOR
+      this.loaded = true
+    },
   },
 })
 </script>
