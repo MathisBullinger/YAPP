e6e21b7214471940f95db4a46ec86855f7462a00
MathisBullinger <mathis.bullinger@code.berlin>
2019-07-29 21:29:38 +0200

    [dev] (feat): toggle play button

--- src/components/molecular/Player.vue
+++ src/components/molecular/Player.vue
@@ -1,17 +1,33 @@
 <template>
   <div :class="getClass">
-    <audio controls src="http://traffic.libsyn.com/hellointernet/126.mp3">
-      Your browser does not support the
-      <code>audio</code> element.
-    </audio>
+    <audio src="http://traffic.libsyn.com/hellointernet/126.mp3"></audio>
+    <div class="controls">
+      <!-- <Icon :name="playState ? 'pause' : 'play'" @click="togglePlay"></Icon> -->
+      <PlayButton v-model="playState"></PlayButton>
+    </div>
   </div>
 </template>
 
 <script>
 import Component from '~scripts/component'
+import PlayButton from './player/PlayButton'
 
 export default new Component({
   name: 'Player',
+  components: {
+    PlayButton,
+  },
+  data() {
+    return {
+      audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+      playState: false,
+    }
+  },
+  watch: {
+    playState(v) {
+      console.log(v)
+    },
+  },
 })
 </script>
 
@@ -26,5 +42,18 @@ export default new Component({
   box-sizing: border-box;
   border-bottom: 1px solid rgba(#000, 8%);
   @include pseudoShadow(7);
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .controls {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    justify-content: space-around;
+    align-items: center;
+  }
 }
 </style>
---
+++ src/components/molecular/player/PlayButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button data-playing="false" role="switch" aria-checked="false" @click="toggle">
+    <Icon :name="value ? 'pause' : 'play'"></Icon>
+  </button>
+</template>
+
+<script>
+import Component from '~scripts/component'
+
+export default new Component({
+  name: 'PlayButton',
+  data() {
+    return {
+      value: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.value = !this.value
+      this.$emit('input', this.value)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
