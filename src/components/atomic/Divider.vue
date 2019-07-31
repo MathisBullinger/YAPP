83d65b884c86a3b53a9603827f8560c0fbe66f64
MathisBullinger <mathis.bullinger@code.berlin>
2019-07-31 19:29:33 +0200

    [dev] (feat): add divider component

---
+++ src/components/atomic/Divider.vue
@@ -0,0 +1,20 @@
+<template>
+  <hr />
+</template>
+
+<script>
+import Component from '~scripts/component'
+
+export default new Component({
+  name: 'Divider',
+})
+</script>
+
+<style lang="scss" scoped>
+hr {
+  margin: 0;
+  height: 1px;
+  border: none;
+  background-color: gray;
+}
+</style>
