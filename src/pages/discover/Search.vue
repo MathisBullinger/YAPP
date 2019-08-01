ab2d7640c84b4ef04fb3dfabce9318344e7a5399
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-01 19:54:59 +0200

    [dev] (feat): search expansion transition

--- src/pages/discover/Search.vue
+++ src/pages/discover/Search.vue
@@ -5,6 +5,8 @@
       @focus="$parent.$emit('keyboard', true)"
       @blur="$parent.$emit('keyboard', false)"
     />
+    <div class="input-shadow"></div>
+    <div class="expanded"></div>
   </div>
 </template>
 
@@ -13,11 +15,17 @@ import Component from '~scripts/component'
 
 export default new Component({
   name: 'Search',
+  data() {
+    return {
+      active: false,
+    }
+  },
 })
 </script>
 
 <style lang="scss" scoped>
 @import '~/styles/shadows';
+$trans: 0.2s linear;
 
 .search {
   width: 100%;
@@ -29,14 +37,60 @@ export default new Component({
     width: 100%;
     margin: 0;
     border: none;
-    transition: box-shadow 0.2s ease;
     height: 100%;
+  }
+
+  .input-shadow {
+    box-shadow: none;
+    pointer-events: none;
+  }
+
+  .expanded {
+    z-index: 3000;
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    left: 0;
+    background-color: white;
+    height: 0;
+    display: block;
+    transition: height $trans, transform $trans, width $trans,
+      box-shadow 0s linear 0.2s;
+    box-shadow: none;
+  }
+  .input:focus ~ .expanded {
+    transform: none;
+    width: 100%;
+    height: calc(100vh - 8rem);
+    box-shadow: shadow(4);
+    transition: height $trans, transform $trans, width $trans, box-shadow 0s;
   }
 }
 
 .merged > .search {
-  .input {
+  .input-shadow {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: block;
+    transform: translateY(-100%);
     box-shadow: shadow(2);
+    border-radius: 0.25rem;
+    transition: transform $trans, width $trans, height $trans,
+      border-radius 0s 0.2s;
+  }
+
+  .input:focus ~ .input-shadow {
+    transform: translateX(-1rem) translateY(calc(-100% + 0.5rem));
+    width: calc(100% + 2rem);
+    height: calc(100% + 1rem);
+    border-radius: 0;
+    transition: transform $trans, width $trans, height $trans, border-radius 0s;
+  }
+
+  .expanded {
+    width: calc(100% - 2rem);
+    transform: translateX(1rem) translateY(-0.5rem);
   }
 }
 </style>
