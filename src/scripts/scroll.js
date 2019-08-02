c650b70c157a661e7c8bf730cdfb6f6d58d076be
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-02 22:08:02 +0200

    [scroll] (feat): scroll monitor subscriber system

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,24 +1,77 @@
 import { throttle, debounce } from 'lodash'
 
+const DEBUG = process.env.NODE_ENV === 'development'
+
 class ScrollMonitor {
   constructor() {
-    this.interval = 100
     this._listener = function() {
       this._throttled()
     }.bind(this)
+
+    this._subscribers = {
+      scroll: [],
+      dirChange: [],
+    }
   }
 
-  start() {
+  subscribe(handler, event, minInterval) {
+    if (!Object.keys(this._subscribers).includes(event))
+      throw Error(`invalid event type ${event}`)
+
+    if (!this._subscribers[event].map(sub => sub.handler).includes(handler))
+      this._subscribers[event].push({ handler, interval: minInterval })
+
+    if (!this._interval || minInterval < this._interval)
+      this.interval = minInterval
+
+    if (Object.values(this._subscribers).flat().length === 1) this._start()
+  }
+
+  unsubscribe(handler, event) {
+    if (!Object.keys(this._subscribers).includes(event))
+      throw Error(`invalid event type ${event}`)
+
+    const subscriber = this._subscribers[event].find(
+      sub => sub.handler === handler
+    )
+    if (!subscriber) return
+    this._subscribers[event].splice(
+      this._subscribers[event].indexOf(subscriber),
+      1
+    )
+
+    if (Object.values(this._subscribers).flat().length === 0) this._stop()
+
+    if (subscriber.interval === this._interval) {
+      const newMinInterval = Math.min(
+        ...Object.values(this._subscribers)
+          .flat()
+          .map(sub => sub.interval)
+      )
+      if (newMinInterval > this._interval) this.interval = newMinInterval
+    }
+  }
+
+  _start() {
+    this._lastY = window.scrollY
+    this._lastDeltaY = 0
     window.addEventListener('scroll', this._listener, { passive: true })
+    /* eslint-disable-next-line no-console */
+    if (DEBUG) console.log(`[ScrollMonitor]: start`)
   }
 
-  stop() {
+  _stop() {
     window.removeEventListener('scroll', this._listener)
+    delete this._interval
+    /* eslint-disable-next-line no-console */
+    if (DEBUG) console.log(`[ScrollMonitor]: stop`)
   }
 
   set interval(v) {
     this._interval = v
     this._throttled = this._createThrottled()
+    /* eslint-disable-next-line no-console */
+    if (DEBUG) console.log(`[ScrollMonitor]: interval ${v} ms`)
   }
 
   _createThrottled() {
@@ -26,15 +79,20 @@ class ScrollMonitor {
   }
 
   _handler() {
-    console.log('scroll')
+    const dy = window.scrollY - this._lastY
+    this._subscribers.scroll.forEach(sub =>
+      sub.handler({ y: window.scrollY, dy })
+    )
+    if (dy > 0 !== this._lastDeltaY > 0)
+      this._subscribers.dirChange.forEach(sub =>
+        sub.handler({ y: window.scrollY, dy })
+      )
+    this._lastY = window.scrollY
+    this._lastDeltaY = dy
   }
 }
 
 const scrollMonitor = new ScrollMonitor()
-
-scrollMonitor.start()
-setTimeout(() => (scrollMonitor.interval = 1000), 2000)
-setTimeout(() => scrollMonitor.stop(), 5000)
 
 export default {
   addEventListener: ({ event, handler }) => {},
