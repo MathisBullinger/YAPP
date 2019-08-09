654cc674a5bbd609fa725bc0ebeb671b1606ab90
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-09 12:21:11 +0200

    component bundling for different file types

--- src/components/molecular/index.js
+++ src/components/molecular/index.js
@@ -1,12 +1,11 @@
 import imp from './**.*'
 
-const atoms = Object.fromEntries(
+const molecules = Object.fromEntries(
   Object.entries(imp)
     .filter(([k]) => k !== 'index')
     .map(([k, v]) => [k, v[Object.keys(v)[0]].default])
 )
 
-const Text = atoms.Text
-const Page = atoms.Page
-const Title = atoms.Title
-export { Text, Page, Title }
+const Mainnav = molecules.Mainnav
+export { Mainnav }
+
