474851613f7201b50cd28c7164eef3b85e2629c4
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-10 12:54:25 +0200

    abstract component utility class

---
+++ src/utils/component.ts
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assert } from '~/utils/debug'
+
+export default abstract class Component extends React.Component {
+  protected props: {
+    children: any
+  }
+  protected variant: string
+
+  constructor(props: object, variants?: string[], defaultVariant?: string) {
+    super(props)
+    if (!variants) return
+    assert(
+      () => variants.every(v => /[a-z]/.test(v[0])),
+      'variants must be lower case'
+    )
+    const variantProps = Object.keys(props).filter(p => variants.includes(p))
+    assert(
+      () => variantProps.length > 0 || variants.includes(defaultVariant),
+      'no variant specified'
+    )
+    assert(() => variantProps.length < 2, 'multiple variants specified')
+    if (variantProps.length > 0) this.variant = variantProps[0]
+    else this.variant = defaultVariant
+  }
+
+  abstract render()
+}
