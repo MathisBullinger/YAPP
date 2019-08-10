e7c390c06b474306e74a3707bd495c1cac2306a2
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-10 06:12:47 +0200

    add labeled component

---
+++ src/components/molecular/Labeled.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default class Labeled extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <LabelStyled>{this.props.children}{this.props.for}</LabelStyled>
+    )
+  }
+}
+
+const LabelStyled = styled.label`
+  display: block;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 1rem;
+
+  * {
+    vertical-align: middle;
+    margin-left: 1rem;
+  }
+`
