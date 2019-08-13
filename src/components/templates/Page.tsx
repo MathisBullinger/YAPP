4198b8ae98bb8d1fd2cc6c06e1a67f4503b1692b
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-13 10:35:07 +0200

    (ref): rewrite page as function component

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -2,35 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 import { responsive, layout } from '~/styles'
 
-export default class Page extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const Page: React.FunctionComponent = props => <S.Page>{props.children}</S.Page>
+export default Page
 
-  render() {
-    return (
-      <PageTag responsive={responsive} layout={layout}>
-        {this.props.children}
-      </PageTag>
-    )
-  }
+namespace S {
+  export const Page = styled.div`
+    padding: 4rem;
+    padding-top: 2rem;
+    min-height: calc(100vh - 4rem);
+    margin-bottom: 4rem;
+    box-sizing: border-box;
+    background-color: ${({ theme }) => theme.clBackground};
+
+    @media ${() => responsive.navOnSide} {
+      margin-left: ${() => layout.desktop.navWidth};
+      min-height: 100vh;
+      margin-bottom: 0;
+    }
+
+    @media ${() => responsive.navCollapsed} {
+      margin-left: ${() => layout.desktop.navWidthCollapsed};
+    }
+  `
 }
-
-const PageTag = styled.div`
-  padding: 4rem;
-  padding-top: 2rem;
-  min-height: calc(100vh - 4rem);
-  margin-bottom: 4rem;
-  box-sizing: border-box;
-  background-color: ${({ theme }) => theme.clBackground};
-
-  @media ${({ responsive }) => responsive.navOnSide} {
-    margin-left: ${({ layout }) => layout.desktop.navWidth};
-    min-height: 100vh;
-    margin-bottom: 0;
-  }
-
-  @media ${({ responsive }) => responsive.navCollapsed} {
-    margin-left: ${({ layout }) => layout.desktop.navWidthCollapsed};
-  }
-`
