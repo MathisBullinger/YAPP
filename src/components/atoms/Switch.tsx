bdb26766e38e2b49fc418781d4239a185b5c9423
MathisBullinger <mathis.bullinger@code.berlin>
2019-08-15 14:52:44 +0200

    rewrite switch to render as button

--- src/components/atoms/Switch.tsx
+++ src/components/atoms/Switch.tsx
@@ -6,102 +6,111 @@ interface Props {
   value?: 'on' | 'off'
   onInput?(v: boolean): void
   inset?: boolean
+  name?: string
 }
 
-const Switch: FunctionComponent<Props> = ({
-  value = 'off',
-  onInput,
-  inset = false,
-}) => (
-  <ThemeProvider theme={{ ...(!inset && { topic: 'primary' }) }}>
-    <SwitchStyled
-      className={inset ? 'inset' : ''}
-      data-value={value}
-      aria-checked={value === 'on' ? 'true' : 'false'}
+const Switch: FunctionComponent<Props> = props => (
+  <ThemeProvider theme={{ ...(!props.inset && { topic: 'primary' }) }}>
+    <S.Switch
+      className={props.inset ? 'inset' : ''}
+      data-value={props.value || 'off'}
+      aria-checked={props.value === 'on' ? 'true' : 'false'}
+      name={props.name}
       onClick={({ target }: { target: any }) => {
         target.setAttribute(
           'aria-checked',
           target.dataset.value === 'on' ? 'false' : 'true'
         )
         target.dataset.value = target.dataset.value === 'on' ? 'off' : 'on'
-        if (onInput) onInput(target.dataset.value === 'on')
+        if (props.onInput) props.onInput(target.dataset.value === 'on')
       }}
     />
   </ThemeProvider>
 )
 export default Switch
 
-const SwitchStyled = styled.div`
-  display: inline-block;
-  width: 2rem;
-  height: 0.8rem;
-  border-radius: 0.4rem;
-  cursor: pointer;
-  transition: background-color ${() => timing.colorSwap};
-  background-color: ${({ theme }) =>
-    theme
-      .surface()
-      .on()
-      .substring(0, 7)}88;
-
-  &:after {
-    content: '';
+namespace S {
+  const scale = 1
+  const height = 1
+  export const Switch = styled.button`
     display: block;
-    margin: 0;
-    margin-top: -0.1rem;
-    width: 1rem;
-    height: 1rem;
-    border-radius: 0.5rem;
+    position: relative;
+    width: ${2 * height * scale}rem;
+    height: ${0.8 * height * scale}rem;
+    border-radius: ${0.4 * height * scale}rem;
+    padding: 0;
+    border: none;
+    cursor: pointer;
+    transition: background-color ${() => timing.colorSwap};
     background-color: ${({ theme }) =>
-      !theme.invertAction
-        ? theme.surface().color
-        : theme
-            .surface()
-            .on()
-            .substring(0, 7)};
-    box-shadow: ${shadow(1)};
-    transition: transform 0.15s ease, background-color ${() => timing.colorSwap};
-  }
+      theme
+        .surface()
+        .on()
+        .substring(0, 7)}88;
 
-  &[data-value='on'] {
-    background-color: ${({ theme }) =>
-      `${theme[theme.topic](theme.variant).color}${
-        theme.invertAction ? '88' : ''
-      }`};
-
-    &:after {
-      transform: translateX(100%);
-      background-color: ${({ theme }) =>
-        theme.invertAction
-          ? theme[theme.topic](theme.variant).color
-          : theme.surface().color};
+    &:focus {
+      outline: none;
     }
-  }
-
-  &.inset {
-    height: 1.1rem;
-    width: 2.2rem;
-    border-radius: 0.6rem;
-    background-color: ${({ theme }) =>
-      theme[theme.topic](theme.variant).on('disabled')};
 
     &:after {
-      height: 0.9rem;
-      width: 0.9rem;
-      border-radius: 0.45rem;
-      margin: 0.1rem;
-      box-shadow: none;
+      content: '';
+      display: block;
+      width: ${height * scale}rem;
+      height: ${height * scale}rem;
+      border-radius: 50%;
       background-color: ${({ theme }) =>
-        theme[theme.topic](theme.variant).color};
+        !theme.invertAction
+          ? theme.surface().color
+          : theme
+              .surface()
+              .on()
+              .substring(0, 7)};
+      box-shadow: ${shadow(1)};
+      transition: transform 0.15s ease,
+        background-color ${() => timing.colorSwap};
+      position: absolute;
+      transform: translateY(-50%) translateX(-10%);
     }
 
     &[data-value='on'] {
       background-color: ${({ theme }) =>
-        theme[theme.topic](theme.variant).on('high')};
+        `${theme[theme.topic](theme.variant).color}${
+          theme.invertAction ? '88' : ''
+        }`};
 
       &:after {
-        transform: translateX(calc(100% + 0.2rem));
+        transform: translateY(-50%) translateX(calc(10% + ${height * scale}rem));
+        background-color: ${({ theme }) =>
+          theme.invertAction
+            ? theme[theme.topic](theme.variant).color
+            : theme.surface().color};
       }
     }
-  }
-`
+
+    &.inset {
+      height: ${height * scale}rem;
+      border-radius: ${1 * height * scale}rem;
+      background-color: ${({ theme }) =>
+        theme[theme.topic](theme.variant).on('disabled')};
+
+      &:after {
+        height: ${0.75 * scale}rem;
+        width: ${0.75 * scale}rem;
+        box-shadow: none;
+        background-color: ${({ theme }) =>
+          theme[theme.topic](theme.variant).color};
+        transform: translateY(-50%) translateX(17%);
+      }
+
+      &[data-value='on'] {
+        background-color: ${({ theme }) =>
+          theme[theme.topic](theme.variant).on('high')};
+
+        &:after {
+          transform: translateY(-50%)
+            translateX(calc(${(2 - 0.75) * height * scale}rem - 17%));
+        }
+      }
+    }
+  `
+}
