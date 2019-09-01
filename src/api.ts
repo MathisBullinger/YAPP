ef5b60bab8803a012fc1ece0cf4f666202eb4539
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-01 13:26:59 +0200

    migrate from apollo boost to react apollo

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,24 @@
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
 
 export default new ApolloClient({
-  uri: 'https://83pzen9as1.execute-api.us-east-1.amazonaws.com/dev',
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+      if (networkError) console.log(`[Network error]: ${networkError}`)
+    }),
+    new HttpLink({
+      uri: 'https://83pzen9as1.execute-api.us-east-1.amazonaws.com/dev',
+      credentials: 'same-origin',
+    }),
+  ]),
+  cache: new InMemoryCache(),
 })
