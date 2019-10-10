c83dbab2508e38503426b6feb68aed1ec28d0e8a
MathisBullinger <mathis.bullinger@code.berlin>
2019-10-10 16:32:43 +0200

    key dynamic components

--- src/components/atoms/Dynamic.tsx
+++ src/components/atoms/Dynamic.tsx
@@ -3,28 +3,54 @@ import { Text, Title, Subtitle, Link } from '~/components/atoms'
 import styled from 'styled-components'
 import parse from 'html-react-parser'
 
-function replace(content) {
+function replace(content, layer: number[] = [0]) {
   if (!content) return
   if (!Array.isArray(content)) content = [content]
 
-  return content.map(item => {
+  return content.map((item, i) => {
     const { props, type } = item
+    const nextLayer = [...layer, layer.slice(-1)[0] + 1]
+    const key = [...layer, i].join('-')
     switch (type) {
       case 'p':
-        return <Text>{props.children}</Text>
+        return <Text key={key}>{replace(props.children, nextLayer)}</Text>
       case 'a':
-        return <Link to={props.href}>{props.children}</Link>
+        return (
+          <Link key={key} to={props.href}>
+            {replace(props.children, nextLayer)}
+          </Link>
+        )
       case 'h1':
-        return <Title s4>{props.children}</Title>
+        return (
+          <Title key={key} s4>
+            {replace(props.children, nextLayer)}
+          </Title>
+        )
       case 'h2':
-        return <Title s5>{props.children}</Title>
+        return (
+          <Title key={key} s5>
+            {replace(props.children, nextLayer)}
+          </Title>
+        )
       case 'h3':
-        return <Title s6>{props.children}</Title>
+        return (
+          <Title key={key} s6>
+            {replace(props.children, nextLayer)}
+          </Title>
+        )
       case 'h4':
-        return <Subtitle s1>{props.children}</Subtitle>
+        return (
+          <Subtitle key={key} s1>
+            {replace(props.children, nextLayer)}
+          </Subtitle>
+        )
       case 'h5':
       case 'h6':
-        return <Subtitle s2>{props.children}</Subtitle>
+        return (
+          <Subtitle key={key} s2>
+            {replace(props.children, nextLayer)}
+          </Subtitle>
+        )
       default:
         return item
     }
