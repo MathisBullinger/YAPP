7f87cd5f5b359c4310adde471cf4573ff4a20292
MathisBullinger <mathis@bullinger.dev>
2020-02-03 18:54:35 +0100

    feat: cache images

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,18 +1,32 @@
 export default null
 declare var self: ServiceWorkerGlobalScope
 
-const urlsToCache = ['/index.html', '/app.js', '/manifest.json']
+const urlsToCache = ['/', '/app.js', '/sw.js', '0.worker.js', '/manifest.json']
 
 self.addEventListener('install', (event: any) => {
   self.skipWaiting()
-
   event.waitUntil(caches.open('yapp').then(cache => cache.addAll(urlsToCache)))
 })
 
-self.addEventListener('fetch', (event: any) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => (response ? response : fetch(event.request)))
-  )
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith('https://yapp-images.s3')) {
+    event.respondWith(
+      caches.open('yapp-dynamic').then(cache =>
+        cache.match(event.request).then(
+          response =>
+            response ||
+            fetch(event.request).then(response => {
+              cache.put(event.request, response.clone())
+              return response
+            })
+        )
+      )
+    )
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => (response ? response : fetch(event.request)))
+    )
+  }
 })
